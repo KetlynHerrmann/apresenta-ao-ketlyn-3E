38abc1d54ca6e2e18deab436991bd152b7e6d41e
KetlynHerrmann <163474832+KetlynHerrmann@users.noreply.github.com>
2024-09-03 22:57:55 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const { log } = require("console");
+
+const primeiroNome = "Ketlyn";
+const sobrenome = "Herrmann";
+const idade = 18;
+const cidadeNatal = "São José dos Pinhais"
+const altura = 1.62;
+
+console.log("O meu primeiro nome é:");
+console.log(primeiroNome);
+console.log("o meu sobrenome é:");
+console.log(sobrenome);
+console.log("a minha idade é:");
+console.log(idade);
+console.log("eu nasci na cidade de:");
+console.log(cidadeNatal);
+console.log("a minha altura é:");
+console.log(altura);
